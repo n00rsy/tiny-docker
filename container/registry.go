8035b029a920dddbc2ece619e024c6bcbab6df5e
RidhwaanDev <ridhwaan.any@gmail.com>
2020-07-18 17:25:56 -0400

    add registry. just download a file for now

---
+++ container/registry.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	url := "https://raw.githubusercontent.com/n00rsy/ubernetes/master/ExampleImage.txt"
+	out, err := os.Create("ExampleImage.txt")
+	must(err)
+
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	must(err)
+	defer resp.Body.Close()
+
+	n, err := io.Copy(out, resp.Body)
+	must(err)
+	fmt.Println(n)
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println("Error")
+		panic(err)
+	}
+}
